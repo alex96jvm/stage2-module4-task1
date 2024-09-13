4cbace3c85de1322c7a3fd11afc8f381273bf929
alex96jvm <alex96jvm@gmail.com>
2024-09-13 17:34:25 +0300

    Complete task

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -69,6 +69,52 @@ public class Employee {
     }
 
     public static class EmployeeBuilder {
-        // Write your code here!
+        private String name;
+        private String lastName;
+        private String position;
+        private String phone;
+        private String email;
+        private String carNumber;
+
+        public EmployeeBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public EmployeeBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public EmployeeBuilder setPosition(String position) {
+            this.position = position;
+            return this;
+        }
+
+        public EmployeeBuilder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public EmployeeBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public EmployeeBuilder setCarNumber(String carNumber) {
+            this.carNumber = carNumber;
+            return this;
+        }
+
+        public Employee build(){
+            Employee employee = new Employee();
+            employee.setName(this.name);
+            employee.setLastName(this.lastName);
+            employee.setPosition(this.position);
+            employee.setPhone(this.phone);
+            employee.setEmail(this.email);
+            employee.setCarNumber(this.carNumber);
+            return employee;
+        }
     }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,30 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+import com.mjc.stage2.ConnectionFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+        Properties prop = new Properties();
+
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("h2database.properties")){
+
+            prop.load(input);
+            String url = prop.getProperty("db_url");
+            String name = prop.getProperty("user");
+            String password = prop.getProperty("password");
+            Class.forName(prop.getProperty("jdbc_driver"));
+
+            return DriverManager.getConnection(url, name, password);
+        } catch (IOException | SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
 
